ebef050cd57ab9d83707df841f3ba7d4d21e9c23
Jakub Tomanik <jakub.tomanik@tigerspike.com>
2019-03-08 21:14:04 +0100

    Added basic structure of root flow consisting of: - Root View -- Log In View -- Logged In View -- Loading View -- Error View

---
+++ GPSchedule/GPSchedule/ErrorViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ErrorViewController.swift
+//  GPSchedule
+//
+//  Created by Jakub Tomanik on 08/03/2019.
+//  Copyright © 2019 Jakub Tomanik. All rights reserved.
+//
+
+import Foundation
---
+++ GPSchedule/GPSchedule/LoadingViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LoadingViewController.swift
+//  GPSchedule
+//
+//  Created by Jakub Tomanik on 08/03/2019.
+//  Copyright © 2019 Jakub Tomanik. All rights reserved.
+//
+
+import Foundation
