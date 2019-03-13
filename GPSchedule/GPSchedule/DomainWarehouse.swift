ea1736d8811879a0bf8ee41b032de32ec1db228b
Jakub Tomanik <jakub.tomanik@tigerspike.com>
2019-03-13 22:42:52 +0100

    Added basic implementation of a DomainWarehouse

---
+++ GPSchedule/GPSchedule/DomainWarehouse.swift
@@ -0,0 +1,33 @@
+//
+//  DomainWarehouse.swift
+//  GPSchedule
+//
+//  Created by Jakub Tomanik on 13/03/2019.
+//  Copyright © 2019 Jakub Tomanik. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+class DomainWarehouse: DomainStoreFacade {
+
+    let authService = MockAuthService.self
+    var rootUseCase: RootUseCase!
+
+    init() {
+        self.rootUseCase = RootUseCase(initialState: RootState(), dependencyProvider: self)
+    }
+}
+
+extension DomainWarehouse {
+    func dispatch(event: Event) {
+        switch event {
+        case let event as RootState.DomainEvent:
+            rootUseCase.dispatch(event: event)
+        default:
+            return
+        }
+    }
+}
+
+extension DomainWarehouse: RootUseCaseDependenciesProvider {}
