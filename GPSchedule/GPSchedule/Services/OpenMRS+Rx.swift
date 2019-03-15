d64fdd790b6c03d4f56bdfd392bb0ae3148f477a
Jakub Tomanik <jakub.tomanik@tigerspike.com>
2019-03-15 14:21:23 +0100

    Added RxSwift extension to the OpenMRS client

---
+++ GPSchedule/GPSchedule/Services/OpenMRS+Rx.swift
@@ -0,0 +1,28 @@
+//
+//  OpenMRS+Rx.swift
+//  GPSchedule
+//
+//  Created by Jakub Tomanik on 15/03/2019.
+//  Copyright © 2019 Jakub Tomanik. All rights reserved.
+//
+
+import Foundation
+import OpenMRS
+import RxSwift
+
+extension RequestBuilder {
+    public func rx() -> Observable<T?> {
+        return Observable<T?>.create { observer -> Disposable in
+            self.execute { (response, error) in
+                    if let error = error {
+                        observer.onError(error)
+                    } else {
+                        let result = response?.body
+                        observer.onNext(result)
+                    }
+                    observer.on(.completed)
+            }
+            return Disposables.create()
+        }
+    }
+}
