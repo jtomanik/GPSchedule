7ecb7b8d970ebe630b43348b43c88f21aec70600
Jakub Tomanik <jakub.tomanik@tigerspike.com>
2019-03-18 16:38:46 +0100

    Updated Domain mappings to reflect recent changes in the API client

--- GPSchedule/GPSchedule/Templates/Sourcery.swift
+++ GPSchedule/GPSchedule/Templates/Sourcery.swift
@@ -24,13 +24,22 @@ extension Int: DomainMapable {}
 
 extension UserGet: DomainMapable {}
 extension AppointmentschedulingAppointmentGet: DomainMapable {}
+extension AppointmentschedulingTimeslotGet: DomainMapable {}
 extension AppointmentschedulingTimeslotGetRef: DomainMapable {}
+extension AppointmentschedulingAppointmentblockGet: DomainMapable {}
 extension AppointmentschedulingAppointmentblockGetRef: DomainMapable {}
+extension AppointmentschedulingAppointmenttypeGet: DomainMapable {}
 extension AppointmentschedulingAppointmenttypeGetRef: DomainMapable {}
+extension PatientGet: DomainMapable {}
 extension PatientGetRef: DomainMapable {}
+extension ProviderGet: DomainMapable {}
 extension ProviderGetRef: DomainMapable {}
+extension ProviderGetFull: DomainMapable {}
+extension LocationGet: DomainMapable {}
 extension LocationGetRef: DomainMapable {}
+extension PersonGet: DomainMapable {}
 extension PersonGetRef: DomainMapable {}
+extension PersonGetFull: DomainMapable {}
 
 extension ISOFullDate: DomainMapable {}
 
@@ -42,23 +51,37 @@ extension AppointmentschedulingAppointmentGet: DomainImportable {}
 
 // sourcery: target = "Timeslot"
 extension AppointmentschedulingTimeslotGetRef: DomainImportable {}
+// sourcery: target = "TimeslotShort"
+extension AppointmentschedulingTimeslotGet: DomainImportable {}
 
 // sourcery: target = "AppointmentBlock"
+extension AppointmentschedulingAppointmentblockGet: DomainImportable {}
+// sourcery: target = "AppointmentBlockShort"
 extension AppointmentschedulingAppointmentblockGetRef: DomainImportable {}
 
 // sourcery: target = "AppointmentType"
+extension AppointmentschedulingAppointmenttypeGet: DomainImportable {}
+// sourcery: target = "AppointmentTypeShort"
 extension AppointmentschedulingAppointmenttypeGetRef: DomainImportable {}
 
 // sourcery: target = "Patient"
+extension PatientGet: DomainImportable {}
+// sourcery: target = "PatientShort"
 extension PatientGetRef: DomainImportable {}
 
 // sourcery: target = "Staff"
+extension ProviderGet: DomainImportable {}
+// sourcery: target = "StaffShort"
 extension ProviderGetRef: DomainImportable {}
 
 // sourcery: target = "Location"
+extension LocationGet: DomainImportable {}
+// sourcery: target = "LocationShort"
 extension LocationGetRef: DomainImportable {}
 
 // sourcery: target = "Person"
+extension PersonGet: DomainImportable {}
+// sourcery: target = "PersonShort"
 extension PersonGetRef: DomainImportable {}
 
 // sourcery: target = "FullDate"
