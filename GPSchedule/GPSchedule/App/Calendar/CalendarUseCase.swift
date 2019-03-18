c468e8172604f7959758c3ba22b381625b8304f5
Jakub Tomanik <jakub.tomanik@tigerspike.com>
2019-03-18 16:41:02 +0100

    Updated calendar flow with adjustment to the logic and state transitions

--- GPSchedule/GPSchedule/App/Calendar/CalendarUseCase.swift
+++ GPSchedule/GPSchedule/App/Calendar/CalendarUseCase.swift
@@ -33,55 +33,63 @@ typealias CalendarUseCaseDependenciesProvider = CalendarServiceProvider
 // sourcery: defaultState = "AuthState"
 enum CalendarState: DomainState {
 
+    enum StateError: DomainError, Equatable {
+        case unknown
+        case authError
+    }
+
     enum StateEvent: DomainEvent, Equatable {
         case fetchAll(for: User)
+        case fetched(entries: [Appointment], for: User)
         case fetchDetail(id: String)
 
     }
 
     case all(entires: [Appointment], user: User)
     case detail(entry: Appointment, user: User)
-    case error(DomainError)
+    case error(StateError)
 
     init() {
-        self = .error(DomainError.genericError)
+        self = .error(StateError.unknown)
     }
 }
 
 // sourcery: defaultState = "CalendarState"
 class CalendarUseCase: GenericUseCase<CalendarState> {
 
-//    static func authMiddleware<Service: AuthService>(service: Service.Type) -> DomainStateMiddleware<AuthState> {
-//        return { (event: AuthState.StateEvent) -> Observable<AuthState.StateEvent> in
-//            guard case .login(let username, let password) = event else {
-//                return Observable.just(event)
-//            }
-//            return service.logIn(username: username, password: password)
-//                .execute()
-//                .map { AuthState.StateEvent.loggedIn($0) }
-//                .catchError { (error) -> Single<AuthState.StateEvent> in
-//                    guard let error = error as? AuthError else {
-//                        return Single.just(AuthState.StateEvent.error(.genericError))
-//                    }
-//                    switch error {
-//                    case .unknown:
-//                        return Single.just(AuthState.StateEvent.error(.genericError))
-//                    case .errorMessage(let message):
-//                        return Single.just(AuthState.StateEvent.error(.errorMessage(message)))
-//                    }
-//                }
-//                .asObservable()
-//        }
-//    }
-
-    static func authFeedback() -> DomainStateFeedback<AuthState> {
-        return { (state: AuthState) -> Observable<DomainEvent> in
-            return Observable.empty()
+    static func appointmentsMiddleware<Service: AppointmentsService>(service: Service.Type) -> DomainStateMiddleware<CalendarState> {
+        return { (event: CalendarState.StateEvent) -> Observable<CalendarState.StateEvent> in
+            guard case .fetchAll(let user) = event,
+                let uuid = user.person?.uuid else {
+                return Observable.just(event)
+            }
+            return service.appointments(for: uuid)
+                .execute()
+                .map { values in
+                    return CalendarState.StateEvent.fetched(entries: values, for: user)
+                }.asObservable()
         }
     }
 
+//    static func authFeedback() -> DomainStateFeedback<AuthState> {
+//        return { (state: AuthState) -> Observable<DomainEvent> in
+//            return Observable.empty()
+//        }
+//    }
+
     static func reduce(_ state: CalendarState, _ event: CalendarState.StateEvent) -> CalendarState {
-        return state
+        switch event {
+        case .fetchAll(let user):
+            return state
+        case .fetched(let items, let user):
+            return .all(entires: items, user: user)
+        default:
+            return state
+        }
+    }
+
+    static func errorHandler(_ state: CalendarState, _ error: Error) -> DomainEvent {
+        return RootState.StateEvent.error(RootState.StateError.genericError)
     }
 
     convenience init<DependenciesProvider: CalendarUseCaseDependenciesProvider>(
@@ -90,7 +98,8 @@ class CalendarUseCase: GenericUseCase<CalendarState> {
         dependencyProvider: DependenciesProvider) {
         self.init(warehouse: warehouse,
                   reducer: CalendarUseCase.reduce,
-                  middleware: [],
+                  errorHandler: CalendarUseCase.errorHandler,
+                  middleware: [CalendarUseCase.appointmentsMiddleware(service: dependencyProvider.appointmentsService)],
                   feedbackLoop: [])
     }
 }
