53c551cc6e857c2c3aad09d409f2fe76d8b301dd
Jakub Tomanik <jakub.tomanik@tigerspike.com>
2019-03-19 01:47:05 +0100

    Created time formatter to be used when parsing the dates

---
+++ GPSchedule/GPSchedule/App/Calendar/TimeFormatter.swift
@@ -0,0 +1,34 @@
+//
+//  TimeFormatter.swift
+//  GPSchedule
+//
+//  Created by Jakub Tomanik on 19/03/2019.
+//  Copyright © 2019 Jakub Tomanik. All rights reserved.
+//
+
+import Foundation
+
+class TimeFormatter: DateFormatter {
+    static let shared: TimeFormatter = {
+        let instance = TimeFormatter()
+        instance.locale = Locale(identifier: "en_US_POSIX")
+        instance.dateFormat = "HH:mm"
+        return instance
+    }()
+
+    private override init() {
+        super.init()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func from(date: Date?) -> String {
+        guard let date = date else {
+            return ""
+        }
+
+        return self.string(from: date)
+    }
+}
