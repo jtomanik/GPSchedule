6c8b7c8d555549a14666cbae43028f236020c393
Jakub Tomanik <jakub.tomanik@tigerspike.com>
2019-03-24 22:15:52 +0100

    Added Unit tests and mocks

---
+++ GPSchedule/GPScheduleUnitTests/GenericUseCaseTests.swift
@@ -0,0 +1,9 @@
+//
+//  GenericUseCaseTests.swift
+//  GPScheduleUnitTests
+//
+//  Created by Jakub Tomanik on 23/03/2019.
+//  Copyright © 2019 Jakub Tomanik. All rights reserved.
+//
+
+import Foundation
---
+++ GPSchedule/GPScheduleUnitTests/GenericViewModelTests.swift
@@ -0,0 +1,9 @@
+//
+//  GenericViewModelTests.swift
+//  GPScheduleUnitTests
+//
+//  Created by Jakub Tomanik on 24/03/2019.
+//  Copyright © 2019 Jakub Tomanik. All rights reserved.
+//
+
+import Foundation
